d1ff68a9604713b67c9b3a4236c74f24be57fac3
rakshitreddy7 <rakshitreddy7@gmail.com>
2021-01-14 17:51:00 -0600

    @Qualifier > @Primary

--- src/main/java/com/rakshit/testing/spring/controllers/SpringDemoController.java
+++ src/main/java/com/rakshit/testing/spring/controllers/SpringDemoController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test")
 public class SpringDemoController {
 
+    /*
+    * 1. Major difference between a constructor injection & setter injection is that, constructor injection are mandatory injections (meaning @Autowired(required=false) does not have any impact & you get an error.
+    *    Whereas setter injections are not mandatory.
+    * 2. You can also use @Primary on the service class (either Dog or Cat which implements Animal interface) indicating the prioritized ones.
+    * 3. Interesting point to note is that, @Qualifier has more preference compared to @Primary.
+    * */
+
     private final Animal animal;
 
     public SpringDemoController(@Qualifier(value = "dog") Animal animal) {
--- src/main/java/com/rakshit/testing/spring/models/Cat.java
+++ src/main/java/com/rakshit/testing/spring/models/Cat.java
@@ -1,7 +1,9 @@
 package com.rakshit.testing.spring.models;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
 public class Cat implements Animal {
     @Override
